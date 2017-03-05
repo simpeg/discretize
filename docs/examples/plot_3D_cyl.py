dbe842290df0b5c0af294137d6d21b13421b80f3
Lindsey Heagy <lindseyheagy@gmail.com>
2017-03-05 13:07:26 -0800

    only show one plot in the example

--- docs/examples/plot_3D_cyl.py
+++ docs/examples/plot_3D_cyl.py
@@ -6,7 +6,7 @@ You can use :code:`mesh.plotGrid()` to plot a z and theta slice through a
 3D cyl mesh.
 
 If you wish to plot only one slice, you can use
-:code:`mesh.plotGrid(slice='z')` or :code:`mesh.plotGrid(slice='theta')
+:code:`mesh.plotGrid(slice='z')` or :code:`mesh.plotGrid(slice='theta')`
 
 """
 import discretize
@@ -16,8 +16,8 @@ import matplotlib.pyplot as plt
 def run(plotIt=True):
     mesh = discretize.CylMesh([7, 6, 10])
     mesh.plotGrid()
-    mesh.plotGrid(slice='z')
-    mesh.plotGrid(slice='theta')
+    # mesh.plotGrid(slice='z')
+    # mesh.plotGrid(slice='theta')
 
 if __name__ == '__main__':
     run()
