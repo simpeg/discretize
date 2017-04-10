a46560d3dd7685e77612f96758e97fb047a46e79
Lindsey Heagy <lindseyheagy@gmail.com>
2017-04-10 15:13:18 -0700

    Merge branch 'dev' into feat/3Dcylmesh
    # Conflicts:
    #	requirements_dev.txt

--- tests/base/test_tensor_io.py
+++ tests/base/test_tensor_io.py
@@ -47,6 +47,22 @@ class TestTensorMeshIO(unittest.TestCase):
         os.remove('arange.txt')
         os.remove('arange2.txt')
 
+    def test_read_ubc_mesh(self):
+        fname = os.path.join(os.path.split(__file__)[0], 'ubc_tensor_mesh.msh')
+        mesh = discretize.TensorMesh.readUBC(fname)
+        assert mesh.nCx == 78
+        assert mesh.nCy == 50
+        assert mesh.nCz == 51
+        # spot check a few things in the file
+        assert mesh.hx[0] == 55000
+        assert mesh.hy[0] == 70000
+        # The x0 is in a different place (-z)
+        assert mesh.x0[-1] == 3000 - np.sum(mesh.hz)
+        # the z axis is flipped
+        assert mesh.hz[0] == 20000
+        assert mesh.hz[-1] == 250
+
+
     if has_vtk:
         def test_VTKfiles(self):
             mesh = self.mesh
@@ -62,7 +78,7 @@ class TestTensorMeshIO(unittest.TestCase):
             vecVTK = models['arange.txt']
             assert np.sum(vec - vecVTK) == 0
 
-            print( 'IO of VTR tensor mesh files is working')
+            print('IO of VTR tensor mesh files is working')
             os.remove('temp.vtr')
 
 
