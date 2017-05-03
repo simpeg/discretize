02be5a57cb12eb8109485596cd58e067efd2ae8d
Thibaut Astic <thast@eos.ubc.ca>
2017-05-03 13:29:55 -0700

    add readUBC_DC2DMesh to MeshIO methods

--- discretize/MeshIO.py
+++ discretize/MeshIO.py
@@ -44,6 +44,63 @@ class TensorMeshIO(object):
         # Make the mesh
         tensMsh = TensorMesh([h1, h2, h3], x0)
         return tensMsh
+    
+    @classmethod
+    def readUBC_DC2DMesh(TensorMesh,fileName):
+        """
+        Read UBC GIF 2DTensor mesh and generate 2D Tensor mesh in simpeg
+
+        :param string fileName: path to the UBC GIF mesh file
+        :rtype: Mesh.TensorMesh
+        :return: SimPEG TensorMesh 2D object
+
+        Created on Thu Nov 12 13:14:10 2015
+
+        @author: dominiquef
+
+        """
+
+        fopen = open(fileName, 'r')
+
+        # Read down the file and unpack dx vector
+        def unpackdx(fid, nrows):
+            for ii in range(nrows):
+
+                line = fid.readline()
+                var = np.array(line.split(), dtype=float)
+
+                if ii==0:
+                    x0= var[0]
+                    xvec = np.ones(int(var[2])) * (var[1] - var[0]) / int(var[2])
+                    xend = var[1]
+
+                else:
+                    xvec = np.hstack((xvec, np.ones(int(var[1])) * (var[0] - xend) / int(var[1])))
+                    xend = var[0]
+
+            return x0, xvec
+
+        # Start with dx block
+        # First line specifies the number of rows for x-cells
+        line = fopen.readline()
+        nl = np.array(line.split(), dtype=float)
+        [x0, dx] = unpackdx(fopen, nl)
+        # Move down the file until reaching the z-block
+        line = fopen.readline()
+        if not line:
+            line = fopen.readline()
+        # End with dz block
+        # First line specifies the number of rows for z-cells
+        line = fopen.readline()
+        nl = np.array(line.split(), dtype=float)
+        [z0, dz] = unpackdx(fopen, nl)
+        # Flip z0 to be the bottom of the mesh for SimPEG
+        z0 = z0 - sum(dz)
+        dz = dz[::-1]
+        # Make the mesh
+        tensMsh = TensorMesh([dx, dz], (x0, z0))
+        return tensMsh
+
 
     @classmethod
     def readVTK(TensorMesh, fileName):
