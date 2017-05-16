0783ccf76d79cdba319277a0c52d07f4524281b8
Lindsey Heagy <lindseyheagy@gmail.com>
2017-05-16 12:06:27 -0700

    better error handling on the import of numpy in setup.py as per #51

--- setup.py
+++ setup.py
@@ -6,7 +6,15 @@ Discretization tools for finite volume and inverse problems.
 """
 
 from setuptools import find_packages
-from numpy.distutils.core import setup
+
+try:
+    from numpy.distutils.core import setup
+except Exception:
+    raise Exception(
+        "Install requires numpy. "
+        "If you use conda, `conda install numpy` "
+        "or you can use pip, `pip install numpy`"
+    )
 
 import os
 import sys
