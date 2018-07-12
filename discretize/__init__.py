df57fc483334e23759efd72ff8920868d5ea8ae9
Lindsey Heagy <lindseyheagy@gmail.com>
2018-07-12 12:14:44 -0700

    Bump version: 0.2.0 → 0.2.1

--- discretize/__init__.py
+++ discretize/__init__.py
@@ -18,7 +18,7 @@ except ImportError:
         """
     )
 
-__version__   = '0.2.0'
+__version__   = '0.2.1'
 __author__    = 'SimPEG Team'
 __license__   = 'MIT'
 __copyright__ = '2013 - 2017, SimPEG Developers, http://simpeg.xyz'
