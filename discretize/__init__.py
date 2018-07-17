83e54af52f2214ddc541f69b154c5bb15dcb45ba
Joseph Capriotti <jcapriot@mines.edu>
2018-07-17 17:37:58 -0600

    adding a better error catch with an explanation as to why discretize might not have been imported properly. This seems to be the most common issue for unknown errors.

--- discretize/__init__.py
+++ discretize/__init__.py
@@ -4,7 +4,28 @@ from discretize.CylMesh import CylMesh
 from discretize.CurvilinearMesh import CurvilinearMesh
 from discretize import Tests
 from discretize.MeshIO import load_mesh
-from discretize.TreeMesh import TreeMesh
+try:
+    from discretize.TreeMesh import TreeMesh
+except ImportError as err:
+    print(err)
+    import os
+    # Check if being called from non-standard location (i.e. a git repository)
+    # is tree_ext.cpp here? will not be in the folder if installed to site-packages...
+    file_test = os.path.dirname(os.path.abspath(__file__))+"/tree_ext.pyx"
+    if os.path.isfile(file_test):
+        # Then we are being run from a repository
+        print(
+            """
+            Unable to import TreeMesh.
+
+            It would appear that discretize is being imported from its repository.
+            If this is intentional, you need to run:
+
+            python setup.py build_ext --inplace
+
+            to compile the cython code.
+            """
+            )
 
 __version__   = '0.2.1'
 __author__    = 'SimPEG Team'
