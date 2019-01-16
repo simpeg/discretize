9bea8b519cfa78b1b4436c5efd27b6957b28297e
Lindsey Heagy <lindseyheagy@gmail.com>
2019-01-16 14:07:07 -0800

    update testing of docs to be inline with new build process

--- tests/docs/test_docs.py
+++ tests/docs/test_docs.py
@@ -8,36 +8,65 @@ class Doc_Test(unittest.TestCase):
     @property
     def path_to_docs(self):
         dirname, filename = os.path.split(os.path.abspath(__file__))
-        return os.path.sep.join(dirname.split(os.path.sep)[:-2] + ['docs'])
+        return dirname.split(os.path.sep)[:-2] + ['docs']
+
+    @property
+    def path_to_build(self):
+        return self.path_to_docs + ["_build"]
+
+    @property
+    def path_to_doctrees(self):
+        return self.path_to_build + ["doctrees"]
+
+    @property
+    def path_to_html(self):
+        return self.path_to_build + ["html"]
+
+    @property
+    def path_to_api(self):
+        return self.path_to_build + ["api"]
+
+    def setUp(self):
+
+        subprocess.call([
+           "sphinx-autogen", "-i", "-t", "_templates", "-o",
+           "{}".format(os.path.sep.join(self.path_to_api + ["generated"])),
+           "{}".format(os.path.sep.join(self.path_to_api + ["index.rst"]))
+        ])
 
     def test_html(self):
-        doctrees_path = os.path.sep.join(self.path_to_docs.split(os.path.sep) + ['_build']+['doctrees'])
-        html_path = os.path.sep.join(self.path_to_docs.split(os.path.sep) + ['_build']+['html'])
 
-        check = subprocess.call(["sphinx-build", "-nW", "-b", "html", "-d",
-            "{0!s}".format((doctrees_path)) ,
-            "{0!s}".format((self.path_to_docs)),
-            "{0!s}".format((html_path))])
+        check = subprocess.call([
+            "sphinx-build",
+            # "-nW",
+            "-b", "html", "-d",
+            "{0!s}".format((os.path.sep.join(self.path_to_doctrees))),
+            "{0!s}".format((os.path.sep.join(self.path_to_docs))),
+            "{0!s}".format((os.path.sep.join(self.path_to_html)))
+        ])
         assert check == 0
 
     # def test_latex(self):
-    #     doctrees_path = os.path.sep.join(self.path_to_docs.split(os.path.sep) + ['_build']+['doctrees'])
+    #     path_to_doctrees = os.path.sep.join(self.path_to_docs.split(os.path.sep) + ['_build']+['doctrees'])
     #     latex_path = os.path.sep.join(self.path_to_docs.split(os.path.sep) + ['_build']+['latex'])
 
     #     check = subprocess.call(["sphinx-build", "-nW", "-b", "latex", "-d",
-    #         "%s"%(doctrees_path),
+    #         "%s"%(path_to_doctrees),
     #         "%s"%(self.path_to_docs),
     #         "%s"%(latex_path)])
     #     assert check == 0
 
     def test_linkcheck(self):
-        doctrees_path = os.path.sep.join(self.path_to_docs.split(os.path.sep) + ['_build']+['doctrees'])
-        link_path = os.path.sep.join(self.path_to_docs.split(os.path.sep) + ['_build'])
+        link_path = os.path.sep.join(self.path_to_docs + ['_build'])
 
-        check = subprocess.call(["sphinx-build", "-nW", "-b", "linkcheck", "-d",
-            "%s"%(doctrees_path),
-            "%s"%(self.path_to_docs),
-            "%s"%(link_path)])
+        check = subprocess.call([
+            "sphinx-build",
+            # "-nW",
+            "-b", "linkcheck", "-d",
+            "%s"%(os.path.sep.join(self.path_to_doctrees)),
+            "%s"%(os.path.sep.join(self.path_to_docs)),
+            "%s"%(os.path.sep.join(self.path_to_build + ["linkcheck"]))
+        ])
         assert check == 0
 
 if __name__ == '__main__':
