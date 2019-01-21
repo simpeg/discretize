7fcd95d4fe42792bc88c1ce56ea08325dc9c48d2
Lindsey Heagy <lindseyheagy@gmail.com>
2019-01-21 10:39:35 -0800

    update docs deploy script

--- ci/deploy-docs.sh
+++ ci/deploy-docs.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# To return a failure if any commands inside fail
+set -e
+
 # unpack credentials if they are not already open
 if [ ! -d "credentials" ]; then
     openssl aes-256-cbc -K $encrypted_5813a1339455_key -iv $encrypted_5813a1339455_iv -in credentials.tar.gz.enc -out credentials.tar.gz -d
@@ -9,6 +12,7 @@ fi
 
 # authenticate with gcloud
 cd docs
+echo "Starting deploy of the docs"
 
 # install and setup lib
 conda create -n --yes py27 python=2.7 anaconda
@@ -22,11 +26,14 @@ pip install google-compute-engine
 
 # deploy
 gcloud auth activate-service-account --key-file credentials/client-secret.json
+echo "Successfully authenticated"
 gcloud config set project $GAE_PROJECT
 gcloud app deploy app.yaml --version ${TRAVIS_COMMIT} --promote
+echo "Done deploying docs"
 
 # deactivate python 2.7 environment
 conda deactivate
+cd ../
 
 # Workaround for https://github.com/travis-ci/travis-ci/issues/6522
 # Turn off exit on failure.
