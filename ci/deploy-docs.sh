d86d0a7866b9be2836db3b89c70c30dd721a2e99
Lindsey Heagy <lindseyheagy@gmail.com>
2019-03-18 21:47:41 -0700

    simplify docs deploy

--- ci/deploy-docs.sh
+++ ci/deploy-docs.sh
@@ -17,12 +17,12 @@ cd docs
 echo "Starting deploy of the docs"
 
 # install and setup lib
-conda create -n py27 python=2.7
-source activate py27
-mkdir lib
-pip install -t lib/ flask
-ls lib
-source deactivate
+# conda create -n py27 python=2.7
+# source activate py27
+# mkdir lib
+# pip install -t lib/ flask
+# ls lib
+# source deactivate
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-228.0.0-linux-x86_64.tar.gz
 tar zxvf google-cloud-sdk
 pip install google-compute-engine
