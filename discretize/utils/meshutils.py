cc3ffc5d46c07f22c81e3d3b83719df1374ca96b
fourndo <fourndo@gmail.com>
2019-04-18 08:38:30 -0700

    Formating fixes

--- discretize/utils/meshutils.py
+++ discretize/utils/meshutils.py
@@ -294,13 +294,13 @@ def mesh_builder_xyz(
 
         xyLoc = np.random.randn(8,2)
 
-        mesh = discretize.utils.meshutils.mesh_builder_xyz(xyLoc, [0.1, 0.1],
-                                          depth_core=0.5,
-                                          padding_distance=[[1,2], [1,0]],
-                                          mesh_type='tensor',
+        mesh = discretize.utils.meshutils.mesh_builder_xyz(
+            xyLoc, [0.1, 0.1], depth_core=0.5,
+            padding_distance=[[1,2], [1,0]],
+            mesh_type='tensor',
         )
 
-        # mesh.refine(5, finalize=True)
+
         axs = plt.subplot()
         mesh.plotImage(mesh.vol, grid=True, ax=axs)
         axs.scatter(xyLoc[:,0], xyLoc[:,1], 15, c='w', zorder=3)
