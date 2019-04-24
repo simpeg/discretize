0f1f43811ba4562dc89c0ff86d2eeabbd6d23ec4
Bane Sullivan <banesullivan@gmail.com>
2019-04-24 15:59:15 -0600

    Attempt fix RTD build

--- docs/setup.py
+++ docs/setup.py
@@ -4,12 +4,10 @@ This is a hacked together solutions for running shell scripts on Read the Docs.
 See Pull Request #152 for more details
 """
 
-from subprocess import call
-
-cloneit = "git clone --depth 1 git://github.com/vtkiorg/gl-ci-helpers.git"
-runit = "bash ./gl-ci-helpers/travis/setup_headless_display.sh"
+import os
 
 def setup():
-    call([cloneit, 'sudo', runit])
+    os.system("git clone --depth 1 git://github.com/vtkiorg/gl-ci-helpers.git")
+    os.system("bash ./gl-ci-helpers/travis/setup_headless_display.sh")
 
 setup()
