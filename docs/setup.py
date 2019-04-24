70b045e4c9b1e0e9094b2541efa5025c62efc0e8
Bane Sullivan <banesullivan@gmail.com>
2019-04-24 14:12:57 -0600

    Attemt fix sudo issue on RTD

--- docs/setup.py
+++ docs/setup.py
@@ -6,11 +6,10 @@ See Pull Request #152 for more details
 
 from subprocess import call
 
-COMMANDS = '''git clone --depth 1 git://github.com/vtkiorg/gl-ci-helpers.git;
-bash ./gl-ci-helpers/travis/setup_headless_display.sh;
-'''
+cloneit = "git clone --depth 1 git://github.com/vtkiorg/gl-ci-helpers.git"
+runit = "bash ./gl-ci-helpers/travis/setup_headless_display.sh"
 
 def setup():
-    call(COMMANDS, shell=True)
+    call([cloneit, 'sudo', runit])
 
 setup()
