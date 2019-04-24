1c91b0bf8245c3735d4c822756cc68813110f9c5
Bane Sullivan <banesullivan@gmail.com>
2019-04-24 13:53:43 -0600

    Attempt to fix RTD build for vtki

---
+++ docs/setup.py
@@ -0,0 +1,16 @@
+"""
+This is a hacked together solutions for running shell scripts on Read the Docs.
+
+See Pull Request #152 for more details
+"""
+
+from subprocess import call
+
+COMMANDS = '''git clone --depth 1 git://github.com/vtkiorg/gl-ci-helpers.git;
+source ./gl-ci-helpers/travis/setup_headless_display.sh;
+'''
+
+def setup():
+    call(COMMANDS, shell=True)
+
+setup()
