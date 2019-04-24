e2bd59c2e1027f192faa7a9f0f7c795f07a09c48
Bane Sullivan <banesullivan@gmail.com>
2019-04-24 14:01:21 -0600

    Attempt fix RTD build

--- docs/setup.py
+++ docs/setup.py
@@ -7,7 +7,7 @@ See Pull Request #152 for more details
 from subprocess import call
 
 COMMANDS = '''git clone --depth 1 git://github.com/vtkiorg/gl-ci-helpers.git;
-source ./gl-ci-helpers/travis/setup_headless_display.sh;
+bash ./gl-ci-helpers/travis/setup_headless_display.sh;
 '''
 
 def setup():
