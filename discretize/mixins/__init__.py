145aca7926dcbc9e4850159e1aaef2b5302ae9a1
Bane Sullivan <banesullivan@gmail.com>
2019-05-13 12:05:41 -0600

    Fix Python 2 issue

--- discretize/mixins/__init__.py
+++ discretize/mixins/__init__.py
@@ -20,8 +20,34 @@ except ImportError as err:
     pass
 
 
-class InterfaceMixins(*AVAILABLE_MIXIN_CLASSES):
-    """This class handles all the avaialble mixins that can be inherrited
-    directly onto ``discretize.BaseMesh``
-    """
-    pass
+# NOTE: this is what we need to use when Python 2 support is dropped
+#       This throws a syntax error on Python 2
+# # Python 3 friendly
+# class InterfaceMixins(*AVAILABLE_MIXIN_CLASSES):
+#     """This class handles all the avaialble mixins that can be inherrited
+#     directly onto ``discretize.BaseMesh``
+#     """
+#     pass
+
+# Python 2 is tedious
+# NOTE: this will have to be updated anytime a mixin is added
+# NOTE: this should be deleted when Python 2 support is dropped
+if len(AVAILABLE_MIXIN_CLASSES) == 0:
+    class InterfaceMixins():
+        """This class handles all the avaialble mixins that can be inherrited
+        directly onto ``discretize.BaseMesh``
+        """
+        pass
+elif len(AVAILABLE_MIXIN_CLASSES) == 1:
+    class InterfaceMixins(AVAILABLE_MIXIN_CLASSES[0]):
+        """This class handles all the avaialble mixins that can be inherrited
+        directly onto ``discretize.BaseMesh``
+        """
+        pass
+elif len(AVAILABLE_MIXIN_CLASSES) == 2:
+    class InterfaceMixins(AVAILABLE_MIXIN_CLASSES[0],
+                          AVAILABLE_MIXIN_CLASSES[1]):
+        """This class handles all the avaialble mixins that can be inherrited
+        directly onto ``discretize.BaseMesh``
+        """
+        pass
