92e2a31db12e4abe44165f086a698afca900ad98
dccowan <devinccowan@gmail.com>
2019-05-15 15:46:48 -0700

    fix typo and upgrade git ignore

--- tutorials/mesh_generation/tree_mesh.py
+++ tutorials/mesh_generation/tree_mesh.py
@@ -61,10 +61,10 @@ xp, yp = np.meshgrid([120., 240.], [80., 160.])
 xy = np.c_[matutils.mkvc(xp), matutils.mkvc(yp)]
 
 # Discretize to finest cell size within rectangular box
-mesh2 = meshutils.refine_tree_xyz(mesh, xy, octree_levels=[2, 2],
+mesh = meshutils.refine_tree_xyz(mesh, xy, octree_levels=[2, 2],
                                   method='box', finalize=False)
 
-mesh2.finalize()  # Must finalize tree mesh before use
+mesh.finalize()  # Must finalize tree mesh before use
 
 mesh.plotGrid(showIt=True)
 
@@ -149,8 +149,8 @@ mesh = TreeMesh([hx, hy], x0='CC')
 xx = mesh.vectorNx
 yy = -3*np.exp((xx**2) / 100**2) + 50.
 pts = np.c_[matutils.mkvc(xx), matutils.mkvc(yy)]
-mesh = meshutils.refine_tree_xyz(mesh, pts, octree_levels=[2, 2],
-                                 method='surface', finalize=False)
+#mesh = meshutils.refine_tree_xyz(mesh, pts, octree_levels=[2, 2],
+#                                 method='surface', finalize=False)
 
 # Refine near points
 xx = np.array([0., 10., 0., -10.])
@@ -214,8 +214,8 @@ mesh = TreeMesh([hx, hy, hz], x0='CCC')
 [xx, yy] = np.meshgrid(mesh.vectorNx, mesh.vectorNy)
 zz = -3*np.exp((xx**2 + yy**2) / 100**2) + 50.
 pts = np.c_[matutils.mkvc(xx), matutils.mkvc(yy), matutils.mkvc(zz)]
-mesh = meshutils.refine_tree_xyz(mesh, pts, octree_levels=[2, 2],
-                                 method='surface', finalize=False)
+#mesh = meshutils.refine_tree_xyz(mesh, pts, octree_levels=[2, 2],
+#                                 method='surface', finalize=False)
 
 # Refine box
 xp, yp, zp = np.meshgrid([-40., 40.], [-40., 40.], [-60., 0.])
