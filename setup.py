cd7c205266e338c70ebf50ec15c506c03d7427f2
Lindsey Heagy <lindseyheagy@gmail.com>
2019-07-08 14:11:04 -0700

    Bump version: 0.4.7 → 0.4.8

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ def configuration(parent_package='', top_path=None):
 
 setup(
     name="discretize",
-    version="0.4.7",
+    version="0.4.8",
     install_requires=[
         'numpy>=1.8',
         'scipy>=0.13',
