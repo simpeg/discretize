477bef0ac02e16b8e1c57ef691c0a25f0a0e3ee4
Joseph Capriotti <jcapriot@mines.edu>
2020-05-26 12:24:17 -0600

    add example within code

--- discretize/utils/meshutils.py
+++ discretize/utils/meshutils.py
@@ -808,6 +808,38 @@ def active_from_xyz(mesh, xyz, grid_reference='CC', method='linear'):
 
     active : numpy.ndarray
         1D mask array of `bool` for the active cells below xyz.
+
+    Examples
+    --------
+
+    .. plot::
+        :include-source:
+
+        import matplotlib.pyplot as plt
+        import numpy as np
+        from discretize import TensorMesh
+        from discretize.utils import active_from_xyz
+
+        mesh = TensorMesh([5, 5])
+        topo_func = lambda x: -3*(x-0.2)*(x-0.8)+.5
+        topo_points = np.linspace(0, 1)
+        topo_vals = topo_func(topo_points)
+
+        active_cc = active_from_xyz(mesh, np.c_[topo_points, topo_vals], grid_reference='CC')
+        ax = plt.subplot(121)
+        mesh.plotImage(active_cc, ax=ax)
+        mesh.plotGrid(centers=True, ax=ax)
+        ax.plot(np.linspace(0,1), topo_func(np.linspace(0,1)), color='C3')
+        ax.set_title("CC")
+
+        active_n = active_from_xyz(mesh, np.c_[topo_points, topo_vals], grid_reference='N')
+        ax = plt.subplot(122)
+        mesh.plotImage(active_n, ax=ax)
+        mesh.plotGrid(nodes=True, ax=ax)
+        ax.plot(np.linspace(0,1), topo_func(np.linspace(0,1)), color='C3')
+        ax.set_title("N")
+        plt.show()
+
     """
     if isinstance(mesh, discretize.CylMesh):
         raise NotImplementedError('CylMesh is not yet supported')
