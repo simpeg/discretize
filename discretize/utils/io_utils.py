c0380d245f31a8e51a60f8d053a3c88c4e278555
Joseph Capriotti <josephrcapriotti@gmail.com>
2020-09-03 14:00:21 -0700

    Check if explicitly True here

--- discretize/utils/io_utils.py
+++ discretize/utils/io_utils.py
@@ -57,7 +57,7 @@ def download(
                 if verbose:
                     print("overwriting {}".format(download))
             else:
-                while os.path.exists:
+                while os.path.exists is True:
                     download = rename_path(download)
 
                 if verbose:
