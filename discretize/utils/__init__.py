1fb14c30e9878fff7e6bf59e1b30d64c5f537005
Joseph Capriotti <josephrcapriotti@gmail.com>
2020-09-23 11:18:49 -0700

    re-organize imports of deprecated functions

--- discretize/utils/__init__.py
+++ discretize/utils/__init__.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 
 from .code_utils import (is_scalar, as_array_n_by_dim, requires)
+from .code_utils import (isScalar, asArray_N_x_Dim)
+
 from .matrix_utils import (
     mkvc, sdiag, sdinv, speye, kron3, spzeros, ddx, av,
     av_extrap, ndgrid, ind2sub, sub2ind, get_subarray,
@@ -8,27 +10,31 @@ from .matrix_utils import (
     make_property_tensor, inverse_property_tensor, Zero,
     Identity
 )
-from .mesh_utils import (
-    example_curvilinear_grid, mesh_tensor, closest_points, extract_core_mesh,
-    random_model, refine_tree_xyz, active_from_xyz, mesh_builder_xyz
-)
-from .curvilinear_utils import volume_tetrahedron, face_info, index_cube
-from .interpolation_utils import interpolation_matrix, volume_average
-from .coordinate_utils import (
-    rotate_points_from_normals, rotation_matrix_from_normals, cyl2cart, cart2cyl
-    # rotate_vec_cyl2cart
-)
-from .io_utils import download
-
-# DEPRECATIONS
-from .code_utils import (isScalar, asArray_N_x_Dim)
-from .coordinate_utils import (rotationMatrixFromNormals, rotatePointsFromNormals)
-from .curvilinear_utils import (volTetra, indexCube, faceInfo)
-from .interpolation_utils import (interpmat)
 from .matrix_utils import (
     sdInv, getSubArray, inv3X3BlockDiagonal, inv2X2BlockDiagonal,
     makePropertyTensor, invPropertyTensor
 )
+
+from .mesh_utils import (
+    example_curvilinear_grid, mesh_tensor, closest_points, extract_core_mesh,
+    random_model, refine_tree_xyz, active_from_xyz, mesh_builder_xyz
+)
 from .mesh_utils import (
     exampleLrmGrid, meshTensor, closestPoints, ExtractCoreMesh
 )
+
+from .curvilinear_utils import volume_tetrahedron, face_info, index_cube
+from .curvilinear_utils import volTetra, indexCube, faceInfo
+
+from .interpolation_utils import interpolation_matrix, volume_average
+from .interpolation_utils import interpmat
+
+from .coordinate_utils import (
+    rotate_points_from_normals, rotation_matrix_from_normals, cyl2cart, cart2cyl
+    # rotate_vec_cyl2cart
+)
+from .coordinate_utils import (rotationMatrixFromNormals, rotatePointsFromNormals)
+
+from .io_utils import download
+
+# DEPRECATIONS
