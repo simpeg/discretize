f33780ab942a00031c1fb6b1f5b101b54411e178
Joseph Capriotti <josephrcapriotti@gmail.com>
2020-09-23 10:40:00 -0700

    import deprecations into discretize.utils

--- discretize/utils/__init__.py
+++ discretize/utils/__init__.py
@@ -1,22 +1,34 @@
 from __future__ import print_function
 
-from .code_utils import (isScalar, asArray_N_x_Dim, requires)
+from .code_utils import (is_scalar, as_array_n_by_dim, requires)
 from .matrix_utils import (
-    mkvc, sdiag, sdInv, speye, kron3, spzeros, ddx, av,
-    av_extrap, ndgrid, ind2sub, sub2ind, getSubArray,
-    inv3X3BlockDiagonal, inv2X2BlockDiagonal, TensorType,
-    makePropertyTensor, invPropertyTensor, Zero,
+    mkvc, sdiag, sdinv, speye, kron3, spzeros, ddx, av,
+    av_extrap, ndgrid, ind2sub, sub2ind, get_subarray,
+    inverse_3x3_block_diagonal, inverse_2x2_block_diagonal, TensorType,
+    make_property_tensor, inverse_property_tensor, Zero,
     Identity
 )
 from .mesh_utils import (
-    exampleLrmGrid, meshTensor, closestPoints, ExtractCoreMesh,
+    example_curvilinear_grid, mesh_tensor, closest_points, extract_core_mesh,
     random_model, refine_tree_xyz, active_from_xyz, mesh_builder_xyz
 )
-from .curvilinear_utils import volTetra, faceInfo, indexCube
-from .interpolation_utils import interpmat, volume_average
+from .curvilinear_utils import volume_tetrahedron, face_info, index_cube
+from .interpolation_utils import interpolation_matrix, volume_average
 from .coordinate_utils import (
-    rotatePointsFromNormals, rotationMatrixFromNormals, cyl2cart, cart2cyl
+    rotate_points_from_normals, rotation_matrix_from_normals, cyl2cart, cart2cyl
     # rotate_vec_cyl2cart
 )
-
 from .io_utils import download
+
+# DEPRECATIONS
+from .code_utils import (isScalar, asArray_N_x_Dim)
+from .coordinate_utils import (rotationMatrixFromNormals, rotatePointsFromNormals)
+from .curvilinear_utils import (volTetra, indexCube, faceInfo)
+from .interpolation_utils import (interpmat)
+from .matrix_utils import (
+    sdInv, getSubArray, inv3X3BlockDiagonal, inv2X2BlockDiagonal,
+    makePropertyTensor, invPropertyTensor
+)
+from .mesh_utils import (
+    exampleLrmGrid, meshTensor, closestPoints, ExtractCoreMesh
+)
