e788d4d011b4460980b4481f0f8746f5c9bb4308
Joseph Capriotti <josephrcapriotti@gmail.com>
2020-10-09 14:22:27 -0700

    Add deprecations for old utils files.

---
+++ discretize/utils/codeutils.py
@@ -0,0 +1,8 @@
+from discretize.utils.code_utils import *
+import warnings
+
+warnings.warn(
+    "Importing from discretize.codeutils is deprecated behavoir. Please import "
+    "from discretize.utils. This module will be removed in version 1.0.0 of discretize.",
+    FutureWarning
+)
---
+++ discretize/utils/coordutils.py
@@ -0,0 +1,8 @@
+from discretize.utils.coordinate_utils import *
+import warnings
+
+warnings.warn(
+    "Importing from discretize.coordutils is deprecated behavoir. Please import "
+    "from discretize.utils. This module will be removed in version 1.0.0 of discretize.",
+    FutureWarning
+)
