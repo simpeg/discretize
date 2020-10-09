bc146e3ff8a7f16a8aa98abdb2a1a227a57dbb37
Joseph Capriotti <josephrcapriotti@gmail.com>
2020-10-09 13:59:09 -0700

    update deprecations for View

--- discretize/View.py
+++ discretize/View.py
@@ -1,6 +1,6 @@
 from discretize.utils.code_utils import deprecate_module
 
-deprecate_module("View", "mixins.mpl_mod", removal_version="1.0.0")
+deprecate_module("discretize.View", "discretize.mixins.mpl_mod", removal_version="1.0.0")
 try:
     from discretize.mixins.mpl_mod import Slicer
 except ImportError:
