cf41f00a2345ea0f7eabba8e490d17e5816a7c94
Joseph Capriotti <josephrcapriotti@gmail.com>
2020-10-27 17:48:56 -0700

    add DISPLAY and PYVISTA_OFF_SCREEN as variables

--- .ci/setup_headless_display.sh
+++ .ci/setup_headless_display.sh
@@ -4,8 +4,6 @@ set -x
 # sudo apt-get update && sudo apt-get install python-qt4 libgl1-mesa-glx
 sudo apt-get install python-qt4 libgl1-mesa-glx
 sudo apt-get install -y xvfb
-export DISPLAY=:99.0
-export PYVISTA_OFF_SCREEN=True
 which Xvfb
 Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 # Debugging commands:
