4ecff976b3536689dff4c4dda2d97e936aae0840
Joseph Capriotti <josephrcapriotti@gmail.com>
2021-02-26 15:54:52 -0800

    add examples to the documentation for the new refine_box and refine_ball functions

--- discretize/_extensions/tree_ext.pyx
+++ discretize/_extensions/tree_ext.pyx
@@ -414,6 +414,36 @@ cdef class _TreeMesh:
             The level to refine intersecting cells to
         finalize : bool, optional
             Whether to finalize after refining
+
+        Examples
+        --------
+        We create a simple mesh and refine the TreeMesh such that all cells that
+        intersect the spherical balls are at the given levels.
+
+        >>> import discretize
+        >>> import matplotlib.pyplot as plt
+        >>> import matplotlib.patches as patches
+        >>> tree_mesh = discretize.TreeMesh([32, 32])
+        >>> tree_mesh.max_level
+        5
+
+        Next we define the center and radius of the two spheres, as well as the level
+        we want to refine them to, and refine the mesh.
+
+        >>> centers = [[0.1, 0.3], [0.6, 0.8]]
+        >>> radii = [0.2, 0.3]
+        >>> levels = [4, 5]
+        >>> tree_mesh.refine_ball(centers, radii, levels)
+
+        Now lets look at the mesh, and overlay the balls on it to ensure it refined
+        where we wanted it to.
+
+        >>> ax = tree_mesh.plot_grid()
+        >>> circ = patches.Circle(centers[0], radii[0], facecolor='none', edgecolor='r', linewidth=3)
+        >>> ax.add_patch(circ)
+        >>> circ = patches.Circle(centers[1], radii[1], facecolor='none', edgecolor='k', linewidth=3)
+        >>> ax.add_patch(circ)
+        >>> plt.show()
         """
         points = np.require(np.atleast_2d(points), dtype=np.float64,
                             requirements='C')
@@ -451,6 +481,37 @@ cdef class _TreeMesh:
             The level to refine intersecting cells to
         finalize : bool, optional
             Whether to finalize after refining
+
+        Examples
+        --------
+        We create a simple mesh and refine the TreeMesh such that all cells that
+        intersect the boxes are at the given levels.
+
+        >>> import discretize
+        >>> import matplotlib.pyplot as plt
+        >>> import matplotlib.patches as patches
+        >>> tree_mesh = discretize.TreeMesh([32, 32])
+        >>> tree_mesh.max_level
+        5
+
+        Next we define the origins and furthest corners of the two rectangles, as
+        well as the level we want to refine them to, and refine the mesh.
+
+        >>> x0s = [[0.1, 0.1], [0.8, 0.8]]
+        >>> x1s = [[0.3, 0.2], [0.9, 1.0]]
+        >>> levels = [4, 5]
+        >>> tree_mesh.refine_box(x0s, x1s, levels)
+
+        Now lets look at the mesh, and overlay the boxes on it to ensure it refined
+        where we wanted it to.
+
+        >>> ax = tree_mesh.plot_grid()
+        >>> rect = patches.Rectangle([0.1, 0.1], 0.2, 0.1, facecolor='none', edgecolor='r', linewidth=3)
+        >>> ax.add_patch(rect)
+        >>> rect = patches.Rectangle([0.8, 0.8], 0.1, 0.2, facecolor='none', edgecolor='k', linewidth=3)
+        >>> ax.add_patch(rect)
+        >>> plt.show()
+
         """
         x0s = np.require(np.atleast_2d(x0s), dtype=np.float64,
                             requirements='C')
