74ad5c935abd69b7408a0c1ba2d7cc4ed57e3bd9
Joseph Capriotti <josephrcapriotti@gmail.com>
2021-10-21 12:50:34 -0700

    test solely running linkcheck as it requires the html to be built

--- tests/docs/test_docs.py
+++ tests/docs/test_docs.py
@@ -10,18 +10,18 @@ class Doc_Test(unittest.TestCase):
         dirname, file_name = os.path.split(os.path.abspath(__file__))
         return dirname.split(os.path.sep)[:-2] + ["docs"]
 
-    def test_html(self):
-        wd = os.getcwd()
-        os.chdir(os.path.sep.join(self.path_to_docs))
-
-        if platform.system() != "Windows":
-            response = subprocess.run(["make", "html"])
-            self.assertTrue(response.returncode == 0)
-        else:
-            response = subprocess.call(["make", "html"], shell=True)
-            self.assertTrue(response == 0)
-
-        os.chdir(wd)
+    # def test_html(self):
+    #     wd = os.getcwd()
+    #     os.chdir(os.path.sep.join(self.path_to_docs))
+    #
+    #     if platform.system() != "Windows":
+    #         response = subprocess.run(["make", "html"])
+    #         self.assertTrue(response.returncode == 0)
+    #     else:
+    #         response = subprocess.call(["make", "html"], shell=True)
+    #         self.assertTrue(response == 0)
+    #
+    #     os.chdir(wd)
 
     def test_linkcheck(self):
         wd = os.getcwd()
