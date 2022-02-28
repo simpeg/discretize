bcff2c8da34d9532e977e74f4ce01c6ff39fd68d
Joseph Capriotti <josephrcapriotti@gmail.com>
2022-02-28 10:08:11 -0800

    Bump version: 0.7.2 → 0.7.3

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ install_requires = build_requires + [
 
 metadata = dict(
     name="discretize",
-    version="0.7.2",
+    version="0.7.3",
     python_requires=">=3.6",
     setup_requires=build_requires,
     install_requires=install_requires,
