0edfd3789dab434d937b6dcd7b66eef81eaed587
Joseph Capriotti <josephrcapriotti@gmail.com>
2022-08-18 12:33:56 -0700

    fix merge

--- discretize/base/base_tensor_mesh.py
+++ discretize/base/base_tensor_mesh.py
@@ -772,23 +772,6 @@ class BaseTensorMesh(BaseRegularMesh):
     def get_interpolation_matrix(
         self, loc, location_type="cell_centers", zeros_outside=False,
     ):
-<<<<<<< HEAD
-=======
-        if "locType" in kwargs:
-            warnings.warn(
-                "The locType keyword argument has been deprecated, please use location_type. "
-                "This will be removed in discretize 1.0.0",
-                FutureWarning,
-            )
-            location_type = kwargs["locType"]
-        if "zerosOutside" in kwargs:
-            warnings.warn(
-                "The zerosOutside keyword argument has been deprecated, please use zeros_outside. "
-                "This will be removed in discretize 1.0.0",
-                FutureWarning,
-            )
-            zeros_outside = kwargs["zerosOutside"]
->>>>>>> main
         return self._getInterpolationMat(loc, location_type, zeros_outside)
 
     def _fastInnerProduct(
@@ -1003,73 +986,3 @@ class BaseTensorMesh(BaseRegularMesh):
             return innerProductDeriv
         else:
             return None
-<<<<<<< HEAD
-=======
-
-    # DEPRECATED
-    @property
-    def hx(self):
-        """Width of cells in the x direction
-
-        Returns
-        -------
-        numpy.ndarray
-
-        .. deprecated:: 0.5.0
-          `hx` will be removed in discretize 1.0.0 to reduce namespace clutter,
-          please use `mesh.h[0]`.
-        """
-        warnings.warn(
-            "hx has been deprecated, please access as mesh.h[0]", FutureWarning
-        )
-        return self.h[0]
-
-    @property
-    def hy(self):
-        """Width of cells in the y direction
-
-        Returns
-        -------
-        numpy.ndarray or None
-
-        .. deprecated:: 0.5.0
-          `hy` will be removed in discretize 1.0.0 to reduce namespace clutter,
-          please use `mesh.h[1]`.
-        """
-        warnings.warn(
-            "hy has been deprecated, please access as mesh.h[1]", FutureWarning
-        )
-        return None if self.dim < 2 else self.h[1]
-
-    @property
-    def hz(self):
-        """Width of cells in the z direction
-
-        Returns
-        -------
-        numpy.ndarray or None
-
-        .. deprecated:: 0.5.0
-          `hz` will be removed in discretize 1.0.0 to reduce namespace clutter,
-          please use `mesh.h[2]`.
-        """
-        warnings.warn(
-            "hz has been deprecated, please access as mesh.h[2]", FutureWarning
-        )
-        return None if self.dim < 3 else self.h[2]
-
-    vectorNx = deprecate_property("nodes_x", "vectorNx", removal_version="1.0.0", future_warn=True)
-    vectorNy = deprecate_property("nodes_y", "vectorNy", removal_version="1.0.0", future_warn=True)
-    vectorNz = deprecate_property("nodes_z", "vectorNz", removal_version="1.0.0", future_warn=True)
-    vectorCCx = deprecate_property(
-        "cell_centers_x", "vectorCCx", removal_version="1.0.0", future_warn=True
-    )
-    vectorCCy = deprecate_property(
-        "cell_centers_y", "vectorCCy", removal_version="1.0.0", future_warn=True
-    )
-    vectorCCz = deprecate_property(
-        "cell_centers_z", "vectorCCz", removal_version="1.0.0", future_warn=True
-    )
-    isInside = deprecate_method("is_inside", "isInside", removal_version="1.0.0", future_warn=True)
-    getTensor = deprecate_method("get_tensor", "getTensor", removal_version="1.0.0", future_warn=True)
->>>>>>> main
