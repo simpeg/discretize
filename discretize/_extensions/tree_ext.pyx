1eb0ff95558b61cc1b346d72a7c59c99023a9b36
Joseph Capriotti <josephrcapriotti@gmail.com>
2022-11-14 15:39:33 -0800

    update the triangle and tetrahedron documentation with examples

--- discretize/_extensions/tree_ext.pyx
+++ discretize/_extensions/tree_ext.pyx
@@ -783,15 +783,16 @@ cdef class _TreeMesh:
         Next we define the points along the line and the level we want to refine to,
         and refine the mesh.
 
-        >>> segments = np.array([[0.1, 0.3], [0.3, 0.9], [0.8, 0.9]])
+        >>> triangle = [[0.14, 0.31], [0.32, 0.96], [0.23, 0.87]]
         >>> levels = 5
-        >>> tree_mesh.refine_line(segments, levels)
+        >>> tree_mesh.refine_triangle(triangle, levels)
 
         Now lets look at the mesh, and overlay the line on it to ensure it refined
         where we wanted it to.
 
         >>> ax = tree_mesh.plot_grid()
-        >>> ax.plot(*segments.T, color='C1')
+        >>> tri = patches.Polygon(triangle, fill=False)
+        >>> ax.add_patch(tri)
         >>> plt.show()
 
         """
@@ -853,22 +854,27 @@ cdef class _TreeMesh:
         >>> import discretize
         >>> import matplotlib.pyplot as plt
         >>> import matplotlib.patches as patches
-        >>> tree_mesh = discretize.TreeMesh([32, 32])
+        >>> tree_mesh = discretize.TreeMesh([32, 32, 32])
         >>> tree_mesh.max_level
         5
 
         Next we define the points along the line and the level we want to refine to,
         and refine the mesh.
 
-        >>> segments = np.array([[0.1, 0.3], [0.3, 0.9], [0.8, 0.9]])
+        >>> tetra = [
+        ...     [0.32, 0.21, 0.15],
+        ...     [0.82, 0.19, 0.34],
+        ...     [0.14, 0.82, 0.29],
+        ...     [0.32, 0.27, 0.83],
+        ... ]
         >>> levels = 5
-        >>> tree_mesh.refine_line(segments, levels)
+        >>> tree_mesh.refine_tetrahedron(tetra, levels)
 
-        Now lets look at the mesh, and overlay the line on it to ensure it refined
-        where we wanted it to.
+        Now lets look at the mesh, checking how the refine function proceeded.
 
-        >>> ax = tree_mesh.plot_grid()
-        >>> ax.plot(*segments.T, color='C1')
+        >>> levels = tree_mesh.cell_levels_by_index(np.arange(tree_mesh.n_cells))
+        >>> ax = plt.gca()
+        >>> tree_mesh.plot_slice(levels, normal='z', slice_loc=0.2, grid=True, ax=ax)
         >>> plt.show()
 
         """
