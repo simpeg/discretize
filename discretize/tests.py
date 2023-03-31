01fd74b1d711a347caa0cfd7a58c7b94902f5fc8
Joseph Capriotti <josephrcapriotti@gmail.com>
2023-03-31 15:46:32 -0700

    allow plotIt to work if test fails

--- discretize/tests.py
+++ discretize/tests.py
@@ -610,6 +610,13 @@ def check_derivative(
     ========================= PASS! =========================
     Once upon a time, a happy little test passed.
     """
+    # matplotlib is a soft dependencies for discretize,
+    # lazy-loaded to decrease load time of discretize.
+    try:
+        import matplotlib
+        import matplotlib.pyplot as plt
+    except ImportError:
+        matplotlib = False
 
     print("{0!s} checkDerivative {1!s}".format("=" * 20, "=" * 20))
     print(
@@ -651,9 +658,30 @@ def check_derivative(
             )
         )
 
+    @requires({"matplotlib": matplotlib})
+    def _plot_it(axes, passed):
+        if axes is None:
+            axes = plt.subplot(111)
+        axes.loglog(h, E0, "b")
+        axes.loglog(h, E1, "g--")
+        axes.set_title(
+            "Check Derivative - {0!s}".format(("PASSED :)" if passed else "FAILED :("))
+        )
+        axes.set_xlabel("h")
+        axes.set_ylabel("Error")
+        leg = axes.legend(
+            [r"$\mathcal{O}(h)$", r"$\mathcal{O}(h^2)$"],
+            loc="best",
+            title=r"$f(x + h\Delta x) - f(x) - h g(x) \Delta x - \mathcal{O}(h^2) = 0$",
+            frameon=False,
+        )
+        plt.setp(leg.get_title(), fontsize=15)
+        plt.show()
+
     # Ensure we are about precision
     order0 = order0[E0[1:] > eps]
     order1 = order1[E1[1:] > eps]
+
     # belowTol = order1.size == 0 and order0.size >= 0
     # # Make sure we get the correct order
     # correctOrder = order1.size > 0 and np.mean(order1) > tolerance * expectedOrder
@@ -668,6 +696,8 @@ def check_derivative(
             assert np.mean(order1) > tolerance * expectedOrder
         print("{0!s} PASS! {1!s}".format("=" * 25, "=" * 25))
         print(np.random.choice(happiness) + "\n")
+        if plotIt:
+            _plot_it(ax, True)
     except AssertionError as err:
         print(
             "{0!s}\n{1!s} FAIL! {2!s}\n{3!s}".format(
@@ -675,40 +705,9 @@ def check_derivative(
             )
         )
         print(np.random.choice(sadness) + "\n")
+        if plotIt:
+            _plot_it(ax, False)
         raise err
-
-    if plotIt:
-        # matplotlib is a soft dependencies for discretize,
-        # lazy-loaded to decrease load time of discretize.
-        try:
-            import matplotlib
-            import matplotlib.pyplot as plt
-        except ImportError:
-            matplotlib = False
-
-        @requires({"matplotlib": matplotlib})
-        def plot_it(axes):
-            if axes is None:
-                axes = plt.subplot(111)
-            axes.loglog(h, E0, "b")
-            axes.loglog(h, E1, "g--")
-            axes.set_title(
-                "Check Derivative - {0!s}".format(
-                    ("PASSED :)" if passTest else "FAILED :(")
-                )
-            )
-            axes.set_xlabel("h")
-            axes.set_ylabel("Error")
-            leg = axes.legend(
-                [r"$\mathcal{O}(h)$", r"$\mathcal{O}(h^2)$"],
-                loc="best",
-                title=r"$f(x + h\Delta x) - f(x) - h g(x) \Delta x - \mathcal{O}(h^2) = 0$",
-                frameon=False,
-            )
-            plt.setp(leg.get_title(), fontsize=15)
-            plt.show()
-
-        plot_it(ax)
 
     return True
 
