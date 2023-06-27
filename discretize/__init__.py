14cec3a49b0e750a58c9163bcea8e7ddc7255f11
Santiago Soler <santisoler@fastmail.com>
2023-06-27 14:42:28 -0700

    Include TensorCell in docstring of __init__.py

--- discretize/__init__.py
+++ discretize/__init__.py
@@ -18,15 +18,21 @@ Mesh Classes
   TreeMesh
   SimplexMesh
 
-Tree Mesh Cells
-===============
-The :class:`~discretize.tree_mesh.TreeCell` class was designed specificialy to define the cells within tree meshes.
-Instances of :class:`~discretize.tree_mesh.TreeCell` are not meant to be created on there own.
-However, they can be returned directly by indexing a particular cell within a tree mesh.
+Mesh Cells
+==========
+The :class:`~discretize.tensor_cell.TensorCell` and
+:class:`~discretize.tree_mesh.TreeCell` classes were designed specifically to
+define the cells within tensor and tree meshes, respectively.
+Instances of :class:`~discretize.tree_mesh.TreeCell` and
+:class:`~discretize.tensor_cell.TensorCell` are not meant to be created on
+their own.
+However, they can be returned directly by indexing a particular cell within
+a tensor or tree mesh.
 
 .. autosummary::
   :toctree: generated/
 
+  tensor_cell.TensorCell
   tree_mesh.TreeCell
 """
 
