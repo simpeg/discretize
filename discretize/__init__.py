74a3d77bf63e826ceb14c4a2ea5009501b5fa3df
Joseph Capriotti <josephrcapriotti@gmail.com>
2023-09-11 14:27:03 -0600

    remove generated version file

--- discretize/__init__.py
+++ discretize/__init__.py
@@ -72,13 +72,15 @@ __author__ = "SimPEG Team"
 __license__ = "MIT"
 __copyright__ = "2013 - 2023, SimPEG Developers, https://simpeg.xyz"
 
+from importlib.metadata import version, PackageNotFoundError
+
 # Version
 try:
     # - Released versions just tags:       0.8.0
     # - GitHub commits add .dev#+hash:     0.8.1.dev4+g2785721
     # - Uncommitted changes add timestamp: 0.8.1.dev4+g2785721.d20191022
-    from discretize.version import version as __version__
-except ImportError:
+    __version__ = version("discretize")
+except PackageNotFoundError:
     # If it was not installed, then we don't know the version. We could throw a
     # warning here, but this case *should* be rare. discretize should be
     # installed properly!
