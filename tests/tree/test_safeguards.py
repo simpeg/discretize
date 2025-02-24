f22890171499ab0ab02afdd0917be5d4266801f5
Santiago Soler <santisoler@fastmail.com>
2025-02-24 15:37:58 -0800

    Extend tests

--- tests/tree/test_safeguards.py
+++ tests/tree/test_safeguards.py
@@ -30,7 +30,43 @@ def refine_mesh(mesh):
     mesh.refine_box(p1, p2, levels=5, finalize=False)
 
 
-PROPERTIES = ["h_gridded"]
+PROPERTIES = [
+    "average_cell_to_face",
+    "average_cell_to_face_x",
+    "average_cell_to_face_y",
+    "average_cell_to_face_z",
+    "average_cell_vector_to_face",
+    "boundary_edges",
+    "boundary_face_outward_normals",
+    "boundary_faces",
+    "boundary_nodes",
+    "cell_centers",
+    "cell_volumes",
+    "edge_lengths",
+    "edges_x",
+    "edges_y",
+    "edges_z",
+    "face_areas",
+    "faces_x",
+    "faces_y",
+    "faces_z",
+    "h_gridded",
+    "hanging_edges_x",
+    "hanging_edges_y",
+    "hanging_edges_z",
+    "hanging_faces_x",
+    "hanging_faces_y",
+    "hanging_faces_z",
+    "hanging_nodes",
+    "nodes",
+    "project_edge_to_boundary_edge",
+    "project_face_to_boundary_face",
+    "project_node_to_boundary_node",
+    "stencil_cell_gradient_x",
+    "stencil_cell_gradient_y",
+    "stencil_cell_gradient_z",
+    "total_nodes",
+]
 
 
 class TestSafeGuards:
