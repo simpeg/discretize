ab214ecd722088507c323843f4958185a7827ada
Joseph Capriotti <josephrcapriotti@gmail.com>
2025-09-24 09:11:01 -0600

    update ci script

--- .ci/azure/setup_env.sh
+++ .ci/azure/setup_env.sh
@@ -4,6 +4,7 @@ set -ex #echo on and exit if any line fails
 # TF_BUILD is set to True on azure pipelines.
 is_azure=$(echo "${TF_BUILD:-false}" | tr '[:upper:]' '[:lower:]')
 do_doc=$(echo "${DOC_BUILD:-false}" | tr '[:upper:]' '[:lower:]')
+is_free_threaded=$(echo "${PYTHON_FREETHREADING:-false}" | tr '[:upper:]' '[:lower:]')
 
 if ${is_azure}
 then
@@ -14,7 +15,12 @@ then
 fi
 
 cp .ci/environment_test.yml environment_test_with_pyversion.yml
-echo "  - python="$PYTHON_VERSION >> environment_test_with_pyversion.yml
+if ${is_free_threaded}
+then
+  echo "  - python-freethreading="$PYTHON_VERSION >> environment_test_with_pyversion.yml
+else
+  echo "  - python="$PYTHON_VERSION >> environment_test_with_pyversion.yml
+fi
 
 conda env create --file environment_test_with_pyversion.yml
 rm environment_test_with_pyversion.yml
